8d9963fcdad95f6cc19b2d003f149af42f9f938a
Minsuk1014 <godldy73@gmail>
2024-10-01 21:34:38 +0900

    Remove Assets/AssetsManage from tracking

--- Assets/AssetsManage/SPUM/Script/SPUM_PackageElement.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class SPUM_PackageElement : MonoBehaviour
-{
-    public Text AnimName;
-    public Button PlayButton;
-    public Toggle SelectButton;
-}
